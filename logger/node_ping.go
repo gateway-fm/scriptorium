53b68595480c50daecbf95e9f795338cbc5d512c
Serega <65994110+Misnaged@users.noreply.github.com>
2022-02-17 14:37:47 +0300

    Node ping (#11)
    * ps-477. return from proto body
    
    * node pinging service util added and refactored
    
    * interface args fixed. go mod go sum updated
    
    * ima live replaced
    
    * ``!` removed
    
    Co-authored-by: Misnaged <Fvtyj2327473>

---
+++ logger/node_ping.go
@@ -0,0 +1,35 @@
+package logger
+
+import (
+	"context"
+	"fmt"
+)
+
+type INodePing interface {
+	LogNodeRequestSend(ctx context.Context, isHealthCheck bool, bc, id, addr string)
+	LogNodeRequestHandled(ctx context.Context, isHealthCheck bool, bc, id, addr string)
+}
+
+type NodePing struct{}
+
+// LogNodeRequestSend logs a message about sending the request to a node
+// it marks the log entry as a healthcheck if isHealthCheck is true
+func (n *NodePing) LogNodeRequestSend(ctx context.Context, isHealthCheck bool, bc, id, addr string) {
+	logMessage := fmt.Sprintf("request send to %s-node %s on %s", bc, id, addr)
+	if isHealthCheck {
+		Log().Info("I'm alive")
+		return
+	}
+	LogWithContext(ctx).Info(logMessage)
+}
+
+// LogNodeRequestHandled logs a message about finishing handling the request to a node
+// it marks the log entry as a healthcheck if isHealthCheck is true
+func (n *NodePing) LogNodeRequestHandled(ctx context.Context, isHealthCheck bool, bc, id, addr string) {
+	logMessage := fmt.Sprintf("request handled by  %s-node %s on %s", bc, id, addr)
+	if isHealthCheck {
+		Log().Info("I'm alive")
+		return
+	}
+	LogWithContext(ctx).Info(logMessage)
+}
