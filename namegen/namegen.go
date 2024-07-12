df72071ab48e05797f0e0abb3e292f81dd39bc07
Dmitry Selivanov <dmitry@gateway.fm>
2024-07-12 19:04:55 +0400

    feat: user crypto rand

--- namegen/namegen.go
+++ namegen/namegen.go
@@ -1,12 +1,10 @@
 package namegen
 
 import (
-	"math/rand"
+	"crypto/rand"
+	"math/big"
 	"strings"
-	"time"
 )
-
-var r = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 // NewName generates a name that is strictly less than 25 chars
 func NewName() string {
@@ -22,5 +20,15 @@ func NewName() string {
 
 // oneOf selects a random element from a list.
 func oneOf[T any](list []T) T {
-	return list[r.Intn(len(list))]
+	if len(list) <= 0 {
+		var zero T
+		return zero
+	}
+
+	maxVal := big.NewInt(int64(len(list)))
+	// don't need to check err there, it only occurs if maxVal <= 0
+	// but this case is handled in return above
+	randomBigInt, _ := rand.Int(rand.Reader, maxVal)
+
+	return list[int(randomBigInt.Int64())]
 }
