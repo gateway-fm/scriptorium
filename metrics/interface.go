eeade04e73c853409b414597e86b83157eba89d4
Iusupov Anton <yusupovanton@outlook.com>
2024-08-03 13:32:54 +0300

    feat: not sanitizing dots in metrics names

--- metrics/interface.go
+++ metrics/interface.go
@@ -2,6 +2,7 @@ package metrics
 
 import "github.com/prometheus/client_golang/prometheus"
 
+// Registry defines the interface for managing metrics.
 type Registry interface {
 	Inc(series Series, status string)
 	RecordDuration(series Series, duration float64)
