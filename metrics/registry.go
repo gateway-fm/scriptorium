cacae51ca6acc8ec8d7fd8fbff5f0816e95b4ede
Anton Iusupov <166704638+antoniusupov@users.noreply.github.com>
2024-08-03 18:06:19 +0300

    New levels approach (#35)
    * feat: not sanitizing strings for metrics
    
    * feat: not sanitizing dots in metrics names
    
    * feat: new levels approach in metrics
    
    ---------
    
    Co-authored-by: Iusupov Anton <yusupovanton@outlook.com>

--- metrics/registry.go
+++ metrics/registry.go
@@ -18,6 +18,7 @@ type registry struct {
 	histograms map[string]*prometheus.HistogramVec
 }
 
+// NewRegistry creates a new metrics registry with the specified subsystem and namespace.
 func NewRegistry(subsystem, namespace string) Registry {
 	r := &registry{
 		Subsystem:    subsystem,
@@ -36,32 +37,31 @@ func (r *registry) sanitizeMetricName(name string) string {
 	return strings.ReplaceAll(name, "-", "_")
 }
 
-func (r *registry) Inc(series Series, status string) {
+// Inc increments a counter for the given Series.
+func (r *registry) Inc(name string, labels prometheus.Labels) {
 	r.metricsMu.Lock()
 	defer r.metricsMu.Unlock()
 
-	metricName, labels := series.SuccessLabels()
-	labels["status"] = status
-	sanitized := r.sanitizeMetricName(metricName)
+	sanitized := r.sanitizeMetricName(name)
 	counter, exists := r.counters[sanitized]
 	if !exists {
 		counter = prometheus.NewCounterVec(prometheus.CounterOpts{
 			Subsystem: r.Subsystem,
 			Namespace: r.Namespace,
 			Name:      sanitized,
-		}, []string{"series_type", "name", "operation", "status"})
+		}, []string{"series_type", "sub_type", "operation", "status", "error_code"})
 		r.PromRegistry.MustRegister(counter)
 		r.counters[sanitized] = counter
 	}
 	counter.With(labels).Inc()
 }
 
-func (r *registry) RecordDuration(series Series, duration float64) {
+// RecordDuration records a duration for the given Series.
+func (r *registry) RecordDuration(name string, labels prometheus.Labels, duration float64) {
 	r.metricsMu.Lock()
 	defer r.metricsMu.Unlock()
 
-	metricName, labels := series.DurationLabels()
-	sanitized := r.sanitizeMetricName(metricName)
+	sanitized := r.sanitizeMetricName(name)
 	histogram, exists := r.histograms[sanitized]
 	if !exists {
 		histogram = prometheus.NewHistogramVec(prometheus.HistogramOpts{
@@ -69,13 +69,14 @@ func (r *registry) RecordDuration(series Series, duration float64) {
 			Namespace: r.Namespace,
 			Name:      sanitized,
 			Buckets:   prometheus.DefBuckets,
-		}, []string{"series_type", "name", "operation"})
+		}, []string{"series_type", "sub_type", "operation"})
 		r.PromRegistry.MustRegister(histogram)
 		r.histograms[sanitized] = histogram
 	}
 	histogram.With(labels).Observe(duration)
 }
 
+// PrometheusRegistry returns the underlying Prometheus registry.
 func (r *registry) PrometheusRegistry() *prometheus.Registry {
 	return r.PromRegistry
 }
